fff36a14656e3bce817204136dda54ccbb0bad9e
yajindra.gautam <yajindra.gautam@ekbana.info>
2023-02-03 13:15:51 +0545

    connect databse sqlite using typeorm

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-
+import {TypeOrmModule} from '@nestjs/typeorm'
+import { User } from './user.entity'; // User entity is licke user table
 @Module({
+  imports:[TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
   providers: [UsersService]
 })
